a334ef60f08c36e1bba7120acd03df949a1b8a0f
yxyRain <yxyMashiro@gmail.com>
2020-07-08 00:46:23 +0800

    semi-finished products

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 class INGREDIENTS{//食材类
     private : 
-        static int _num;
+        static int _num;//统计食材种类
     protected :
         double price;//基类价格
-        static int num;//食材数量
+    //  static int num;//食材数量
         string name;//食材名字
     public :
         INGREDIENTS(){ };
         INGREDIENTS(double _price, string _name);
         ~INGREDIENTS(){ };
+        virtual void print()
+        {
+            cout << " INGREDIENTS's number is " <<  _num << "and This INGREDIENTS's name is " << name << " and the price is " << price << endl;
+        }
 };
 
 int INGREDIENTS::_num = 0;
@@ -24,12 +28,16 @@ INGREDIENTS::INGREDIENTS(double _price, string _name) : price(_price), name(_nam
 
 class MEAT : public INGREDIENTS{//肉类派生
     public :
-        string species;
-        string level;
-        string origin;
+        string species;//派生： 肉类种类
+        string level;//派生： 肉类等级
+        string origin;//派生： 肉类产地
+        void print();
 };
 
 class AQUATIC_PRODUCTS : public INGREDIENTS{//水产品派生
+    public : 
+        string origin;//派生: 产地
+        string 
 };
 
 class VRGETABLES : public INGREDIENTS{//蔬菜类派生
